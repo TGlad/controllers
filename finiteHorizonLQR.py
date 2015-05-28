4909858ca955d5bdc98f06125a28b29e11ce2375
TGlad <t_d_lowe@hotmail.com>
2015-05-28 20:15:39 +1000

    new

--- finiteHorizonLQR.py
+++ finiteHorizonLQR.py
@@ -9,11 +9,16 @@ This is a temporary script file.
 import matplotlib.pyplot as plt
 import scipy.integrate as it
 import numpy as np
-j = 1 # scales in x (or time)
-def order2(Y,t):
-    return [Y[1], -(b*Y[1]+c*(Y[0]))/a]
+def ricatti(P):
+    # sympy has a matrix class with matrices in
+    return A.transpose()*P+P*A - (P*B+N)*R.inverse()*(B.transpose()*P*N.transpose() + Q;
+
+def finiteHorizonLQR(A,B,F,Q,R): # ignoring the N matrix, not usually needed
+    
+    
+
 
 time = np.arange(0, 15/j, 0.01) 
-pos,vel,acc,jerk = it.odeint(order4,[1.0,0.0,0.0,0.0],time).T 
+Ps = it.odeint(order4,[0.0],time).T 
 axes[2].plot(time, -vel, color='k')
 axes[2].set_title('order 4')
